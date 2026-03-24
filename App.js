71f1a4b98fd7829d43e535f7f1b73837b8b16949
yrdma <artuzoygor@gmail.com>
2026-03-24 20:43:03 -0300

    CodigoFuncionando

--- App.js
+++ App.js
@@ -2,16 +2,14 @@
 import { useEffect, useState } from 'react';
 import { Text, View, StyleSheet, TextInput, Button, FlatList } from 'react-native';
 
-
-
-
 export default function App() {
   const [dados, setDados] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const buscarDados = async () => {
     try {
       const resposta = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const dados = await resposta.jason();
+      const dados = await resposta.json();
       setDados(dados);
     } catch (erro) {
       console.error('Erro ao Buscar dados:', erro)
@@ -19,31 +17,38 @@ export default function App() {
     } finally {
       setLoading(false);
     }
+  }
+  useEffect(() => {
+    buscarDados()
+  }, []);
 
-    useEffect(() => {
-
-    }, []);
-
-    if (loanding) {
-      return (
-        <View style={styles.container}>
-          <Text>Carregando...</Text>
-        </View>
-      );
-    }
+  if (loading) {
     return (
       <View style={styles.container}>
-        <FlatList
-
-        />
+        <Text>Carregando...</Text>
       </View>
     );
-
-
   }
 
 
-  return ();
+
+
+
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={dados}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.body}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 
 }
 const styles = StyleSheet.create({
