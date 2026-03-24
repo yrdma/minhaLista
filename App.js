e07556c3af6291e18fa9ae4df39817db6b7d8a7a
yrdma <artuzoygor@gmail.com>
2026-03-24 20:20:16 -0300

    aulaApi

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, TextInput, Button, FlatList } from 'react-native';
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [dados, setDados] = useState([])
 
+  const buscarDados = async () => {
+    try {
+      const resposta = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const dados = await resposta.jason();
+      setDados(dados);
+    } catch (erro) {
+      console.error('Erro ao Buscar dados:', erro)
+
+    } finally {
+      setLoading(false);
+    }
+
+    useEffect(() => {
+
+    }, []);
+
+    if (loanding) {
+      return (
+        <View style={styles.container}>
+          <Text>Carregando...</Text>
+        </View>
+      );
+    }
+    return (
+      <View style={styles.container}>
+        <FlatList
+
+        />
+      </View>
+    );
+
+
+  }
+
+
+  return ();
+
+}
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
+
+
+
+
+
